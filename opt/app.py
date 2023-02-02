6cb5951532462b94b6328008ae562962ab92de5f
na0ki_mac <wehallo53.en@gmail.com>
2023-02-02 16:38:33 +0900

    input one file

--- opt/app.py
+++ opt/app.py
@@ -1,8 +1,21 @@
 import streamlit as st
+import tempfile
+from pathlib import Path
+import os
 def main():
     st.title("viewsmooth")
     st.caption("[View source code](https://github.com/na0ki-y/viewsmooth)")
-    pass
+    uploaded_file = st.file_uploader("Choose a file")
+    files = os.listdir("/")
+    print(files)
+    if uploaded_file is not None:
+        with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
+            st.markdown("## Original PDF file")
+            fp = Path(tmp_file.name)
+            fp.write_bytes(uploaded_file.getvalue())
+            st.write(tmp_file.name)
+
+    title = st.text_input('filepath',"/home/user/prog/result/")
 
 if __name__=="__main__":
     main()
