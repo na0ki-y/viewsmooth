c34c9e3aea3148e86c3c757ac68fb036c3a54530
na0ki_mac <wehallo53.en@gmail.com>
2023-02-02 16:17:55 +0900

    init

---
+++ opt/app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+def main():
+    st.title("viewsmooth")
+    st.caption("[View source code](https://github.com/na0ki-y/viewsmooth)")
+    pass
+
+if __name__=="__main__":
+    main()
